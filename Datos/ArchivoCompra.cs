109d283216f8029357cdb3479c2e9eae5ac76c49
juantaborda27 <juantabordaacosta@gmail.com>
2024-04-23 22:23:26 -0500

    Se crearon clases ArchivoCompra y ArchivoProducto

---
+++ Datos/ArchivoCompra.cs
@@ -0,0 +1,75 @@
+﻿using ENTIDADES;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Datos
+{
+    public class ArchivoCompra : Icrud<Compra>
+    {
+        private readonly string fileName = "Compra.bin";
+        FileStream archivo;
+        public void Add(Compra compra)
+        {
+            List<Compra> compras = Leer();
+            compras.Add(compra);
+            Guardar(compras);
+        }
+        public void Guardar(List<Compra> compras)
+        {
+            archivo = new FileStream(fileName, FileMode.OpenOrCreate);
+            BinaryFormatter formatter = new BinaryFormatter();
+            formatter.Serialize(archivo, compras);
+            archivo.Close();
+        }
+        public List<Compra> Leer()
+        {
+            archivo = new FileStream(fileName, FileMode.OpenOrCreate);
+            BinaryFormatter formatter = new BinaryFormatter();
+            List<Compra> compras = (List<Compra>)formatter.Deserialize(archivo);
+            archivo.Close();
+            return compras;
+        }
+        public bool Eliminar(string IdCompra)
+        {
+            List<Compra> compras = Leer();
+            Compra compra = Buscar(IdCompra);
+            if (compra != null)
+            {
+                compras.Remove(compra);
+                Guardar(compras);
+                return true;
+            }
+            return false;
+        }
+        public Compra Buscar(string IdCompra)
+        {
+            List<Compra> compras = Leer();
+            foreach (var item in compras)
+            {
+                if (item.IdCompra.Equals(IdCompra))
+                {
+                    return item;
+                }
+            }
+            return null;
+        }
+        public bool Actualizar(Compra compraNew)
+        {
+            List<Compra> compras = Leer();
+            Compra compraOld = Buscar(compraNew.IdCompra);
+            if (compraOld != null)
+            {
+                compras.Remove(compraOld);
+                compras.Add(compraNew);
+                Guardar(compras);
+                return true;
+            }
+            return false;
+        }
+    }
+}
---
+++ Datos/ArchivoProducto.cs
@@ -0,0 +1,76 @@
+﻿using ENTIDADES;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Datos
+{
+    public class ArchivoProducto:Icrud<Producto>
+    {
+        private readonly string fileName = "Producto.bin";
+        FileStream archivo;
+        public void Add(Producto producto)
+        {
+            List<Producto> productos = Leer();
+            productos.Add(producto);
+            Guardar(productos);
+        }
+        public void Guardar(List<Producto> productos)
+        {
+            archivo = new FileStream(fileName, FileMode.OpenOrCreate);
+            BinaryFormatter formatter = new BinaryFormatter();
+            formatter.Serialize(archivo, productos);
+            archivo.Close();
+        }
+        public List<Producto> Leer()
+        {
+            archivo = new FileStream(fileName, FileMode.OpenOrCreate);
+            BinaryFormatter formatter = new BinaryFormatter();
+            List<Producto> productos = (List<Producto>)formatter.Deserialize(archivo);
+            archivo.Close();
+            return productos;
+        }
+        public bool Eliminar(string idProducto)
+        {
+            List<Producto> productos = Leer();
+            Producto producto = Buscar(idProducto);
+
+            if (producto != null)
+            {
+                productos.Remove(producto);
+                Guardar(productos);
+                return true;
+            }
+            return false;
+        }
+        public Producto Buscar(string idProducto)
+        {
+            List<Producto> productos = Leer();
+            foreach (var item in productos)
+            {
+                if (item.idProducto.Equals(idProducto))
+                {
+                    return item;
+                }
+            }
+            return null;
+        }
+        public bool Actualizar(Producto productoNew)
+        {
+            List<Producto> productos = Leer();
+            Producto productoOld = Buscar(productoNew.idProducto);
+            if (productoOld != null)
+            {
+                productos.Remove(productoOld);
+                productos.Add(productoNew);
+                Guardar(productos);
+                return true;
+            }
+            return false;
+        } 
+    }
+}
