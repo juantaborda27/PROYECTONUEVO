2052fce50e3f6aef52af5958ad60a488e11c605e
juantaborda27 <juantabordaacosta@gmail.com>
2024-04-25 22:13:50 -0500

    Se creo la LogicaLogin

--- Logica/LogicaLogin.cs
+++ Logica/LogicaLogin.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Datos;
+using ENTIDADES;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,33 @@ using System.Threading.Tasks;
 
 namespace Logica
 {
-    internal class LogicaLogin
+    public class LogicaLogin
     {
+        ArchivoUsuario data;
+        public LogicaLogin()
+        {
+            data = new ArchivoUsuario();
+            //Admin();
+        }
+        public Usuario Loguear(Usuario loguear)
+        {
+            List<Usuario> usuarios = data.Leer();
+            foreach (var item in usuarios)
+            {
+                if (item.userName.Equals(loguear.userName) && (item.contraseña.Equals(loguear.contraseña)))
+                {
+                    return item;
+                }
+            }
+            return null;
+        }
+        //public void Admin()
+        //{
+        //    Usuario administrador = new Usuario();
+        //    administrador.userName = "Admin";
+        //    administrador.contraseña = "1234";
+        //    data.AddUsuario(administrador);
+        //}
+
     }
 }
